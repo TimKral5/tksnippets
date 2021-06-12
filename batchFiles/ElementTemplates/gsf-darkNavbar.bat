63977d218b5ff7792a15b4911374377fbcc69bd1
TimKral5 <TimKral5@outlook.com>
2021-06-12 14:21:27 +0200

    updated package.json and added darkNavbar Snipped

---
+++ batchFiles/ElementTemplates/gsf-darkNavbar.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Admin\OneDrive\Dokumente\Visual Studio 2019\Projects\StringArrayGenerator\StringArrayGenerator\bin\Debug
+StringArrayGenerator.exe "C:\Users\Admin\OneDrive\Dokumente\Visual Studio 2019\Projects\VSCodeExt\HTML-Utils\tksnippets\htmlTemplates\ElementTemplates\darkNavbar.html"
